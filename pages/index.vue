8d5d618f2d7e158704bc0d81ebcbb5a80d2f6ae5
DESKTOP-76V6K1K\se5ng <c9604100@gmail.com>
2022-03-16 23:14:54 +0900

    TODO LIST Project
    with Tyler Potts guide

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,38 @@
 <template>
-  <div>
-    <h1>Hello Nuxters! 👋</h1>
-    <p>
-      This page is rendered on the <strong>{{ rendering }}</strong>
-    </p>
-    <p v-if="rendering === 'server'">
-      First load or hard refresh is done on server side.
-    </p>
-    <p v-if="rendering === 'client'">Navigation is done on client side.</p>
-    <ul>
-      <li>Refresh the page for server side rendering.</li>
-      <li>Click the links to see client side rendering.</li>
-    </ul>
+  <main>
+    <h1>Task Board</h1>
+    <p>Create a list of task</p>
 
-    <NuxtLink to="/about">About Page</NuxtLink>
-  </div>
+    <div class="create-new">
+      <input
+        type="text"
+        v-model="newTask"
+        placeholder="Add a new task"
+        @keypress.enter="addTask"
+      />
+      <button @click="addTask">Add</button>
+    </div>
+
+    <div class="tasks">
+      <Task v-for="(task, i) in $store.state.tasks" :key="i" :task="task" />
+    </div>
+  </main>
 </template>
 
 <script>
 export default {
-  asyncData() {
+  data() {
     return {
-      rendering: process.server ? 'server' : 'client',
+      newTask: '',
     }
+  },
+  methods: {
+    addTask() {
+      if (this.newTask) {
+        this.$store.commit('ADD_TASK', this.newTask)
+        this.newTask = ''
+      }
+    },
   },
 }
 </script>
